742166732588eef72d0fa61a58b2606f1db52d2d
smessiah777 <smessiah17@gmail.com>
2019-09-04 16:57:24 -0500

    basic structure and style done for the projects section

--- src/components/sections/Hero.js
+++ src/components/sections/Hero.js
@@ -4,27 +4,16 @@ import '../sections/Hero.css';
 const Hero = () => {
   return (
     <div>
-      <section id='#' className='section hero'>
-        <div className='nav-overlay'></div>
-        <div className='hero-overlay'>
-          <div className='hero-content-wrap '>
-            <h1 className='name animated fadeInDown slow'>
-              Stephen{' '}
-              <span className='last-name animated fadeInDown slow'>
-                Messiah
-              </span>{' '}
-            </h1>
-            <h2 className='sub-title animated fadeInUp slower'>
-              {' '}
-              Web-Developer
-            </h2>
-          </div>
-
-          <a href='#about' id='enter' className='enter-icon'>
-            <i className='fas fa-caret-down animated fadeInDown slow'></i>
-          </a>
+      <div className='nav-overlay'></div>
+      <div className='hero-overlay'>
+        <div className='hero-content-wrap '>
+          <h1 className='name animated fadeInDown slow'>
+            Stephen{' '}
+            <span className='last-name animated fadeInDown slow'>Messiah</span>{' '}
+          </h1>
+          <h2 className='sub-title animated fadeInUp slower'> Web-Developer</h2>
         </div>
-      </section>
+      </div>
     </div>
   );
 };

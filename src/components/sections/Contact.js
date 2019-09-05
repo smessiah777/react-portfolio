ddc23d1dd5e1c433eaf3d7d4924130f4a6270a7a
smessiah777 <smessiah17@gmail.com>
2019-09-04 20:57:51 -0500

    contact section component created

--- src/components/sections/Contact.js
+++ src/components/sections/Contact.js
@@ -1,7 +1,26 @@
 import React from 'react';
 
 const Contact = () => {
-  return <div></div>;
+  return (
+    <div>
+      <section id='contact' class='section section-content contact-section'>
+        <div class='section-title'>
+          <h3 class='title-h3'>Contact</h3>
+          <i class='fas fa-envelope title-icon'></i>
+        </div>
+        <form id='contact-form' class='contact-form-styles'>
+          <div className='form-section'>
+            <label htmlFor='name-input'>Name</label>
+            <input type='text' id='name' />
+          </div>
+          <div className='form-section'>
+            <label htmlFor='email-input'>Email Address</label>
+            <input type='email' id='email' />
+          </div>
+        </form>
+      </section>
+    </div>
+  );
 };
 
 export default Contact;

5864cfc6eb4ff4010f0d468375de64e77a4b21fb
smessiah777 <smessiah17@gmail.com>
2019-09-04 20:10:32 -0500

    about section component created

--- src/components/sections/Hero.js
+++ src/components/sections/Hero.js
@@ -4,16 +4,23 @@ import '../sections/Hero.css';
 const Hero = () => {
   return (
     <div>
-      <div className='nav-overlay'></div>
-      <div className='hero-overlay'>
-        <div className='hero-content-wrap '>
-          <h1 className='name animated fadeInDown slow'>
-            Stephen{' '}
-            <span className='last-name animated fadeInDown slow'>Messiah</span>{' '}
-          </h1>
-          <h2 className='sub-title animated fadeInUp slower'> Web-Developer</h2>
+      <section id='#' className='section hero'>
+        <div className='nav-overlay'></div>
+        <div className='hero-overlay'>
+          <div className='hero-content-wrap '>
+            <h1 className='name animated fadeInDown slow'>
+              Stephen{' '}
+              <span className='last-name animated fadeInDown slow'>
+                Messiah
+              </span>{' '}
+            </h1>
+            <h2 className='sub-title animated fadeInUp slower'>
+              {' '}
+              Web-Developer
+            </h2>
+          </div>
         </div>
-      </div>
+      </section>
     </div>
   );
 };

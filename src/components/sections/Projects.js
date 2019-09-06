d745da2144290932a0360290bdf70abefcdcb70a
smessiah777 <smessiah17@gmail.com>
2019-09-06 13:54:17 -0500

    adjusted section title

--- src/components/sections/Projects.js
+++ src/components/sections/Projects.js
@@ -4,32 +4,17 @@ import '../sections/Projects.css';
 
 const Projects = () => {
   return (
-    <div>
-<<<<<<< HEAD
-      <div className='container'>
-        <div className='section-title'>
-          <h3 className='title-h3'>Projects</h3>
-          {/* <i className='fas fa-pencil-ruler title-icon'></i> */}
-=======
+    <section>
       <section id='projects' className=' section-content'>
         <div className='section-title'>
           <h3 className='title-h3'>Projects</h3>
           {/* <span><i className='fas fa-pencil-ruler title-icon'></i> </span>*/}
->>>>>>> master
         </div>
-        <h1>I love tacos</h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam,
-          asperiores magnam eos dolorem minima reiciendis sequi est nesciunt
-          nulla animi porro nostrum aperiam! Magni corporis delectus ipsum
-          dolore asperiores iusto.
-        </p>
-<<<<<<< HEAD
-      </div>
-=======
+        <div className='container'>
+          <h1>I love tacos</h1>
+        </div>
       </section>
->>>>>>> master
-    </div>
+    </section>
   );
 };
 
